ba7f83b256ad6a2484869267599632e34eb50084
lucrybpin <lucspinto@outlook.com>
2023-01-21 01:04:45 -0300

    Mouse World Position

--- Assets/Scripts/MouserWorld.cs
+++ Assets/Scripts/MouserWorld.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class MouserWorld : MonoBehaviour
 {
+    private static MouserWorld instance;
+    [SerializeField] private LayerMask mousePlaneLayerMask;
 
-    private void Update()
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public static Vector3 GetPosition()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.Log(Physics.Raycast(ray));
-    }
+        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.mousePlaneLayerMask);
+        return raycastHit.point;
+    } 
 }
